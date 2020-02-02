696cd0f9312627dac6fc968b168c704df267ed7c
Marlon Veiga <marlon_veiga@hotmail.com>
2020-02-02 03:19:07 -0300

    Adicionado componente Header a webview

--- src/containers/ExternalPage.js
+++ src/containers/ExternalPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from '../components/Header';
 import {WebView} from 'react-native-webview';
 
 function ExternalPage({navigation}) {
     const externalUrl = navigation.getParam('external_url');
-    return <WebView style={{flex:1}} source={{uri: `${externalUrl}`}}/>
+    return (
+        <>
+        <Header secao='globo'/>
+    <WebView style={{flex:1}} source={{uri: `${externalUrl}`}}/>
+    </>)
 }
 
 export default ExternalPage;
